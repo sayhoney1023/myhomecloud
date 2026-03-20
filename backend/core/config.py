db26b2d7684e8657b3f5e65f1492bc2082264052
sayhoney1023 <sayhoney1023@naver.com>
2026-03-20 15:46:49 +0900

    feat: Dockerfile 내용 추가

--- backend/core/config.py
+++ backend/core/config.py
@@ -2,4 +2,4 @@ SECRET_KEY = "mhcloud-super-secret-key-change-this-later"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-DATABASE_URL = "postgresql://mhcloud:mhcloud1234@192.168.0.175:5432/mhclouddb"
+DATABASE_URL = "postgresql://mhcloud:mhcloud1234@mhcloud-postgres:5432/mhclouddb"
